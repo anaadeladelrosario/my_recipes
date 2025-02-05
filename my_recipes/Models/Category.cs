f35bc8e9c6974aa333afcf125fe9c256557bd274
Ana <anaadeladelrosario@gmail.com>
2025-02-05 13:17:00 +0100

    adding remaining string-based custom enums

---
+++ my_recipes/Models/Category.cs
@@ -0,0 +1,11 @@
+﻿namespace my_recipes.Models
+
+{
+    public static class Category
+    {
+        public const string Appetizer = "Appetizer";
+        public const string MainCourse = "MainCourse";
+        public const string Dessert = "Dessert";
+        public const string Snack = "Snack";
+    }
+}
---
+++ my_recipes/Models/Cuisine.cs
@@ -0,0 +1,14 @@
+﻿namespace my_recipes.Models
+{
+    public static class Cuisine
+    {
+        public const string Australian = "Australian";
+        public const string Brazilian = "Brazilian";
+        public const string Chinese = "Chinese";
+        public const string Cuban = "Cuban";
+        public const string Italian = "Italian";
+        public const string Japanese = "Japanese";
+        public const string Spanish = "Spanish";
+        public const string Swedish = "Swedish";
+    }
+}
