5e4228f4fcfc0b8309acd23bf9ad8612f7f787e7
Ana <anaadeladelrosario@gmail.com>
2025-02-05 13:23:50 +0100

    adding migration

--- my_recipes/Migrations/20250205121806_stringBasedEnums.Designer.cs
+++ my_recipes/Migrations/20250205121806_stringBasedEnums.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using my_recipes;
 
@@ -11,9 +12,11 @@ using my_recipes;
 namespace my_recipes.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250205121806_stringBasedEnums")]
+    partial class stringBasedEnums
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
